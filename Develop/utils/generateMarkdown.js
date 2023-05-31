66c0687b5361d3f151bc6634c10949d85480447c
stevenarroyave8 <stevenarroyave8@gmail.com>
2023-05-30 21:17:50 -0400

    added file for new readme to generate plus functions generate markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -22,12 +22,64 @@ function renderLicenseLink(license) {
 }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+  return `
+  ## License
+  The application is covered under the following license:
+  ${renderLicenseLink(license)}
+    `;
+  } else {
+    return ' ';
+  }
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Table of Contents 
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Description
+
+  ${data.description}
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+  
+
+  ${renderLicenseSection(data.license)}
+  
+  ## Contributing
+  
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.test}
+
+  ## Questions
+  For any questions contact me using the following links:
+
+  [GitHub](https://github.com/${data.githubUsername})
+  
+  [Email: ${data.email}](mailto:${data.email})
 `;
 }
 
---
+++ newREADME/generated-README.md
@@ -0,0 +1,52 @@
+
+  
+  # README generator
+
+  
+  ![badge](https://img.shields.io/badge/license-mit-blue)
+    
+
+  ## Table of Contents 
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Description
+
+  this will generate a readme file for me
+
+  ## Installation
+
+  follow instructions in the repo
+
+  ## Usage
+
+  follow examples on repo 
+  
+
+  
+  ## License
+  The application is covered under the following license:
+  
+  [mit](https://choosealicense.com/licenses/mit)
+    
+    
+  
+  ## Contributing
+  
+  undefined
+
+  ## Tests
+
+  no examples included 
+
+  ## Questions
+  For any questions contact me using the following links:
+
+  [GitHub](https://github.com/stevenarroyave8)
+  
+  [Email: stevenarroyave8](mailto:stevenarroyave8)
