8fee3b99a4c73ee3137f7fd6408f16f509f2ede9
stevenarroyave8 <stevenarroyave8@gmail.com>
2023-05-30 20:43:12 -0400

    added functions for Readme file added inquirer packages again after some issues

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./Develop/utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -89,10 +89,46 @@ const questions = [
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// using the fs to create a new file generated-README in the newREADME folder
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./newREADME/generated-README.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+// using inquirer to generate the .MD file
+const init = () => {
 
-// Function call to initialize app
-init();
+    return inquirer.prompt(questions)
+    .then(readmeData => {
+        return readmeData;
+    })
+}
+init()
+.then(readmeData => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+})
+.then(pageMD => {
+    return writeFile(pageMD);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse.message);
+})
+.catch(err => {
+    console.log(err);
+})
+
+// // Function call to initialize app
+// init();
